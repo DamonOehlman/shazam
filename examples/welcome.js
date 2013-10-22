05eec470c1f93b22b5a265f2168a23440ccef42b
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-22 22:24:45 +1100

    Added examples and readme

---
+++ examples/welcome.js
@@ -0,0 +1,3 @@
+require('..')('Welcome to Shazam', [
+  'What is Shazam?'
+]);
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 /* jshint node: true */
+/* global document: false */
 'use strict';
+
+var crel = require('crel');
+var next = require('dd/next');
+var pull = require('pull-stream');
 
 /**
   # shazam
@@ -7,9 +12,41 @@
 **/
 
 module.exports = function(title, opts, slides) {
+  var past = [];
+  var queued = [];
+  var current;
+
+  function nextSlide() {
+    return current = past[past.length] = queued.shift();
+  }
+
+  function previousSlide() {
+    queued.unshift(current = past.pop());
+
+    return current;
+  }
+
   // check for no opts
   if (Array.isArray(opts)) {
     slides = opts;
     opts = {};
   }
+
+  // copy the deck so we don't destroy it
+  queued = [].concat(slides);
+
+  // set out title based on the title provided
+  document.title = title;
+
+  pull(
+    pull.Source(next('keydown', document)),
+    pull.log()
+  );
+
+  // render the first slide
+  render(nextSlide());
 };
+
+function render(content) {
+  console.log(content);
+}
