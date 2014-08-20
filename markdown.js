3b3cd694979b9d5f8acb55e08ded1cc6d86b4c0d
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-20 23:28:56 +1000

    Overhaul to use marked-ast

--- markdown.js
+++ markdown.js
@@ -1,21 +1,52 @@
-var marked = require('marked');
+// var marked = require('marked');
+var marked = require('marked-ast');
+var Slide = require('./slide');
 var html = require('./html');
 var reSlideBreak = /\n\r?\-{2,}/m;
+// var hljs = require('highlight.js');
 var reLeadingAndTrailingSpaces = /^\s*(.*)\s*$/m;
+
+// var hljsLangMappings = {
+//   js: 'javascript'
+// };
 
 /* initialise marked */
 
-marked.setOptions({
-  highlight: function(code, lang) {
-    return code;
-  }
-});
+// marked.setOptions({
+//   highlight: function(code, lang) {
+//     lang = hljsLangMappings[lang] || lang;
 
-var markdown = module.exports = function(md) {
+//     // if this is a known hljs language then highlight
+//     if (hljs.getLanguage(lang)) {
+//       return hljs.highlight(lang, code).value;
+//     }
+//     else {
+//       return code;
+//     }
+//   }
+// });
+
+var markdown = module.exports = function(md, opts) {
+  var ast;
+  var slide;
+
   // if we have multiple slides, split and map
   if (reSlideBreak.test(md)) {
-    return md.split(reSlideBreak).map(markdown);
+    return md.split(reSlideBreak).map(function(content) {
+      return markdown(content, opts);
+    });
   }
 
-  return html(marked(md.replace(reLeadingAndTrailingSpaces, '$1')));
+  // create the new slide
+  slide = new Slide();
+
+  // parse the slide
+  ast = marked.parse(md.replace(reLeadingAndTrailingSpaces, '$1'));
+
+  // walk the ast and create a slide
+  ast.map(require('marked-ast-crel')).forEach(function(el) {
+    slide.el.appendChild(el);
+  });
+
+  return slide;
 };
