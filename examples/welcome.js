bb47a9e6af4b831a2913c742c6b058a1e0a49b32
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-29 14:59:22 +1100

    Make welcome example consistent with other examples (and general usage)

--- examples/welcome.js
+++ examples/welcome.js
@@ -1,12 +1,11 @@
-var s = require('..');
+var s = require('../slide');
 
-s({
-	title: 'A simple presentation system',
-	slides: [
-		'A simple presentation system',
-		'more test',
-		s.img('http://farm6.staticflickr.com/5350/7053917095_279527795c_o.jpg'),
-		s.img('http://farm4.staticflickr.com/3145/2944068022_19a918b09e_b.jpg'),
-	]
+require('..')({
+  title: 'A simple presentation system',
+  slides: [
+    'A simple presentation system',
+    'more test',
+    s().img('http://farm6.staticflickr.com/5350/7053917095_279527795c_o.jpg'),
+    s().img('http://farm4.staticflickr.com/3145/2944068022_19a918b09e_b.jpg'),
+  ]
 });
-
